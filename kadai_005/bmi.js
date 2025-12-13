142749e9ff8b9a9bb1ea8036c6d2ec4872990d59
ryuichiKobayashi <ryuichi5864@gmail.com>
2025-12-13 15:44:33 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,7 @@
+let bodyweight = 68;
+let height = 1.7;
+let bmi = bodyweight / (height * height);
+
+console.log("体重:", bodyweight, "kg");
+console.log("身長:", height, "m");
+console.log("計算式:", bmi, "[体重(kg)]/[身長(m)*身長(m)]");
